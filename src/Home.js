e5dccb8388770d5765f2fa91eda5b76ac94a9ebd
Om Chaudhari <omchaudhari1904@gmail.com>
2023-10-30 19:07:23 +0530

    2nd Commit

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 import Modal from "./Modal";
 import Sidebar from "./Sidebar";
+import axios from "axios";
 const Home = () => {
   const { openSidebar, openModal } = useGlobalContext();
+  const url =
+    "https://newsapi.org/v2/everything?q=healthcare&sortBy=publishedAt&apiKey=be87e6e7b44b49aeb4e67770508ac240";
+
+  const [Data, setData] = useState([]);
+  const [Loading,setLoading] = useState(false);
+  const getFeeds = async () => {
+    try {
+      const response = await axios.get(url);
+      const result = await response.data;
+      setData(result.articles);
+      console.log(Data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getFeeds();
+  }, []);
+
   return (
     <main>
       <Sidebar />
-      <button className="sidebar-toggle" onMouseEnter={openSidebar}>
-        <FaBars />
-      </button>
-      <div className="home-heading">
-        <h3>Enjoy your latest medical feeds here!!</h3>
+      <div className="feeds">
+        <div className="home-heading">
+          <h3>Get the Latest Medical Updates Here!!</h3>
+        </div>
+        {Data.map((dt,ind) => {
+          const { title, content, urlToImage,url} = dt;
+          return (
+            <div className="feed" key={ind}>
+              <img src={urlToImage}  alt={title}/>
+              <p>{title}...<a href={url}>Know More about it!!</a></p>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
