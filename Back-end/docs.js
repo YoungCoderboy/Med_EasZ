9e00e7009956572633ce6dca71797ac119ea1582
Om Chaudhari <omchaudhari1904@gmail.com>
2023-10-31 22:07:50 +0530

    Final Commit For Today

--- Back-end/docs.js
+++ Back-end/docs.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const fileupload = require("express-fileupload");
-const path=require("path");
+const path = require("path");
 
-const assetPath=path.join(__dirname,"assets");
+const assetPath = path.join(__dirname, "assets");
 
 router.use(fileupload());
 
 router.post("/docs", (req, res) => {
   console.log(req.files);
-  const {file}=req.files;
-  file.mv(path.join(assetPath,file.name));
-  res.status(200).json({ message: "File Uploaded" });
+  try {
+    const { file } = req.files;
+    file.mv(path.join(assetPath, file.name));
+    res.status(200).json({ message: "File Uploaded" });
+  } catch (error) {
+    res.status(500).json({message:"Internal Server Error!!"});
+  }
 });
 
 module.exports = router;
