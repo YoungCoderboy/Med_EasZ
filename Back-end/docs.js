9d8518fe89bef96adee37c5000b7792f4e84fb30
Om Chaudhari <omchaudhari1904@gmail.com>
2023-10-31 21:29:11 +0530

    third commit

--- Back-end/docs.js
+++ Back-end/docs.js
@@ -1,8 +1,16 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const fileupload = require("express-fileupload");
+const path=require("path");
 
-router.post('/docs',(req,res)=>{
-    console.log(req);
-})
+const assetPath=path.join(__dirname,"assets");
 
-module.exports=router;
+router.use(fileupload());
+
+router.post("/docs", (req, res) => {
+  console.log(req.files);
+  const {file}=req.files;
+  res.status(200).json({ message: "File Uploaded" });
+});
+
+module.exports = router;
