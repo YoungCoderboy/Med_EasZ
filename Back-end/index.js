e751b0ec7360e5214763728610201f27097d13e3
YoungCoderboy <91416534+YoungCoderboy@users.noreply.github.com>
2023-11-04 13:22:52 +0530

    Reminder has been added

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,9 +1,18 @@
+const dotenv= require('dotenv');
+dotenv.config({path: './config.env'})
 const express=require("express");
 const app=express();
 const router=require('./docs')
 const bodyparser = require('body-parser')
 const cor=require('cors')
 
+const mongoose = require('mongoose');
+const DB= process.env.DATABASE.replace('<%PASSWORD%>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB).then(()=>{
+  // console.log(con.connection)
+  console.log("connected to database")
+})
 
 app.use(bodyparser.json())
 app.use(express.urlencoded({extended:true}));
--- Front-end/src/pages/Reminder.js
+++ Front-end/src/pages/Reminder.js
@@ -1,7 +1,43 @@
+import { FaPlusCircle } from "react-icons/fa";
+import { useState } from "react";
+
 const Reminder = () => {
+  const [time, setTime] = useState("00:00:00");
+  const [date, setDate] = useState("2023-01-01");
+
+  function changeTime() {
+    var timeInput = document.getElementById("time-input");
+    var clock = document.getElementById("clock");
+    clock.innerHTML = timeInput.value;
+    setTime(timeInput.value);
+  }
+
+  function changeDate() {
+    var dateInput = document.getElementById("date-input");
+    var date = document.getElementById("date");
+    date.innerHTML = dateInput.value.split("-").reverse().join("/");
+
+    setDate(dateInput.value);
+  }
   return (
-    <div>
-      <h2 className="docupload">Set medical reminders</h2>
+    <div className="reminder-main">
+      <input
+        type="time"
+        id="time-input"
+        value={time}
+        onChange={() => changeTime()}
+      />
+      <input
+        type="date"
+        id="date-input"
+        value={date}
+        onChange={() => changeDate()}
+      />
+      <button className="btn">
+        Add <FaPlusCircle />
+      </button>
+
+      <ul></ul>
     </div>
   );
 };
