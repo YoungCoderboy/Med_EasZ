5dd5be7f000884d743f87752336aacd04a9efd03
YoungCoderboy <91416534+YoungCoderboy@users.noreply.github.com>
2023-11-04 22:10:00 +0530

    Update docs.js add route

--- Back-end/docs.js
+++ Back-end/docs.js
@@ -6,11 +6,28 @@ const app = express()
 
 const assetPath = path.join(__dirname, 'assets')
 router.use(express.json())
-router.post('/prediction-data', async (req, res) => {
-  const { body } = req.body
-  console.log(body)
-  res.status(200).json()
-})
+router.post("/prediction-data/:id", async (req, res) => {
+  const { body } = req.body;
+  const object ={
+    simp:body
+  }
+  console.log(object);
+  const newTour = await User.findByIdAndUpdate(req.params.id, object , {
+    new: true,
+    runvalidators: false,
+  });
+  if (!newTour) {
+    return next(new AppError("No tour found for id " + params.id, 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    // result: tours.length,
+    data: {
+      tours: body,
+    },
+  });
+});
 router.use(fileupload())
 
 router.post('/docs', (req, res) => {
