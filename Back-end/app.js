6fb38821a6587b96382a216fbc3196985b1afdd1
unknown <Prathamesh0205>
2023-11-05 23:26:33 +0530

    23:26

--- Back-end/app.js
+++ Back-end/app.js
@@ -6,8 +6,11 @@ const router = require('./docs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+const accountSid = 'ACa7a5f46f69695499e034e20fa2ecc410'
+const authToken = '381f7a1eedf055ec30b1929de96af7ae'
+const client = require('twilio')(accountSid, authToken)
 require('dotenv').config()
-
+const cron = require('node-cron')
 app.use('/', cors(), router)
 
 app.post('/', async (req, res) => {
@@ -72,6 +75,39 @@ app.post('/interests', async (req, res) => {
     console.log(e)
   }
   console.log('done')
+})
+app.post('/rdata', async (req, res) => {
+  console.log(req.body)
+
+  const body = req.body
+  const arr = body.arr.split(' ')
+  const mess = arr[0]
+  const time = arr[1].split(':')
+
+  const weekday = Array(7)
+  weekday['Monday'] = 1
+  weekday['Tuesday'] = 2
+  weekday['Wednesday'] = 3
+  weekday['Thursday'] = 4
+  weekday['Friday'] = 5
+  weekday['Saturday'] = 6
+  weekday['Sunday'] = 0
+
+  // console.log(weekday["Sunday"]);
+  // res.json("Done");
+
+  for (let i = 2; i < arr.length; i++) {
+    cron.schedule(`${time[1]} ${time[0]} * * ${weekday[arr[i]]}`, () => {
+      console.log(mess)
+      client.messages
+        .create({
+          body: mess,
+          from: 'whatsapp:+14155238886',
+          to: 'whatsapp:+917620677425',
+        })
+        .then((message) => console.log(message.sid))
+    })
+  }
 })
 
 app.listen(8000, () => {
--- Front-end/src/pages/Reminder.js
+++ Front-end/src/pages/Reminder.js
@@ -1,48 +1,50 @@
-import { FaPlusCircle } from "react-icons/fa";
-import { useState } from "react";
+import { FaPlusCircle } from 'react-icons/fa'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Reminder = () => {
-  var weekday = new Array(7);
-  weekday[0] = "Monday";
-  weekday[1] = "Tuesday";
-  weekday[2] = "Wednesday";
-  weekday[3] = "Thursday";
-  weekday[4] = "Friday";
-  weekday[5] = "Saturday";
-  weekday[6] = "Sunday";
-  const [value, setValue] = useState([]);
-  const [time, setTime] = useState("12:00");
-  const [date, setDate] = useState([]);
-  const [medName,setMedName]=useState("")
-  function fun(e) {
-    const newArray = date.join(" ");
+  var weekday = new Array(7)
+  weekday[0] = 'Monday'
+  weekday[1] = 'Tuesday'
+  weekday[2] = 'Wednesday'
+  weekday[3] = 'Thursday'
+  weekday[4] = 'Friday'
+  weekday[5] = 'Saturday'
+  weekday[6] = 'Sunday'
+  const [value, setValue] = useState([])
+  const [time, setTime] = useState('12:00')
+  const [date, setDate] = useState([])
+  const [medName, setMedName] = useState('')
+  async function fun(e) {
+    const newArray = date.join(' ')
     // console.log(value);
-    
-    const x=((medName+" "+time+" "+newArray));
-    const newVal=[...value];
+
+    const x = medName + ' ' + time + ' ' + newArray
+    const newVal = [...value]
     console.log(newVal)
     const newx = newVal.filter((it) => {
-        return it !== x;
-      });
+      return it !== x
+    })
     newx.push(x)
-    setValue(newx);
+    setValue(newx)
     // // setValue(value.concat(time+" "+newArray));
     // console.log(value);
-
+    const resp = await axios.post('http://localhost:8000/rdata', {
+      arr: x,
+    })
+    resp()
   }
 
-  function medNameChange(){
-    var MedName=document.getElementById("nameMed")
+  function medNameChange() {
+    var MedName = document.getElementById('nameMed')
     setMedName(MedName)
-
   }
-
 
   function changeTime() {
-    var timeInput = document.getElementById("time-input");
+    var timeInput = document.getElementById('time-input')
     // var clock = document.getElementById("clock");
     // clock.innerHTML = timeInput.value;
-    setTime(timeInput.value);
+    setTime(timeInput.value)
   }
 
   // function changeDate() {
@@ -51,37 +53,44 @@ const Reminder = () => {
   // }
   const handleChange = (e) => {
     if (e.target.checked) {
-      const x = [];
-      x.push(e.target.value);
-      setDate(date.concat(x));
+      const x = []
+      x.push(e.target.value)
+      setDate(date.concat(x))
     } else {
       const newx = date.filter((it) => {
-        return it !== e.target.value;
-      });
-      setDate(newx);
+        return it !== e.target.value
+      })
+      setDate(newx)
     }
-  };
+  }
   return (
     <div className="reminder-main">
-      <input type="text" id="nameMed" value={medName} onChange={(e)=>{setMedName(e.target.value)}} />
+      <input
+        type="text"
+        id="nameMed"
+        value={medName}
+        onChange={(e) => {
+          setMedName(e.target.value)
+        }}
+      />
       <input
         type="time"
         id="time-input"
         value={time}
         onChange={() => changeTime()}
       />
-      <div  className="radio-btn">
+      <div className="radio-btn">
         <ul className="time-ul">
-        {weekday.map((ele,id)=>{
-         return ( <label className="container" key={id}>
-          {ele}
-          <input value={ele} type="checkbox"  onChange={handleChange} />
-          <span className="checkmark"></span>
-        </label>)
-        })}
+          {weekday.map((ele, id) => {
+            return (
+              <label className="container" key={id}>
+                {ele}
+                <input value={ele} type="checkbox" onChange={handleChange} />
+                <span className="checkmark"></span>
+              </label>
+            )
+          })}
         </ul>
-        
-       
       </div>
       <button onClick={() => fun()} className="btn add-data">
         Add
@@ -93,10 +102,10 @@ const Reminder = () => {
             <div className="time-list items" key={id}>
               {element}
             </div>
-          );
+          )
         })}
       </ul>
     </div>
-  );
-};
-export default Reminder;
+  )
+}
+export default Reminder
